f4e7067b5d066f9a69db6b2afce7278505c795e9
shopna akter <shopna88akter@gmail.com>
2024-03-26 12:40:55 +0600

    details route completed succesfully

--- src/Components/BookDetails/Bookdetails.jsx
+++ src/Components/BookDetails/Bookdetails.jsx
@@ -1,6 +1,29 @@
+import { useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
-
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 const Bookdetails = () => {
+    const [readClicked, setReadClicked] = useState(false);
+    const [wishlistClicked, setWishlistClicked] = useState(false);
+    const handleReadClick = () => {
+        if (!readClicked) {
+            toast.success('added in read List');
+            setReadClicked(true);
+        }
+        else{
+            toast.error('already in read list')
+        }
+    };
+
+    const handleWishlistClick = () => {
+        if (!wishlistClicked) {
+            toast.success('added in wishlist');
+            setWishlistClicked(true);
+        }
+        else{
+            toast.error('already in wishlist')
+        }
+    };
     const books = useLoaderData();
     const { id } = useParams()
     const book = books.find(book => book.bookId == id);
@@ -32,8 +55,9 @@ const Bookdetails = () => {
                 <h1 className="pt-4">Year of publishing : <span className="font-bold pl-10">{book.yearOfPublishing}</span></h1>
                 <h1 className="pt-4">Rating : <span className="font-bold pl-[124px]">{book.rating}</span></h1>
                 <div className="flex gap-4 mt-6">
-                    <button className="btn bg-[#1313132d] p-4 font-bold border-white">Read</button>
-                    <button className="btn text-white bg-[#50B1C9]">Wishlist</button>
+                    <button onClick={handleReadClick} className="btn bg-[#1313132d] p-4 font-bold border-white">Read</button>
+                    <button onClick={handleWishlistClick} className="btn text-white bg-[#50B1C9]">Wishlist</button>
+                    <ToastContainer></ToastContainer>
                 </div>
             </div>
         </div>
